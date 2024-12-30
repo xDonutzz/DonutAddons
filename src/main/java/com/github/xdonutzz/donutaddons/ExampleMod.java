9a31d05f39a6b5f2087215888ec4c2fd53a34f95
GitHub Action <action@github.com>
2024-12-30 16:49:12 +0000

    Initialize template repository proper
    Set package name to com.github.xdonutzz.donutaddons
    Set project name to DonutAddons
    Set modid to donutaddons

--- src/main/java/com/github/xdonutzz/donutaddons/ExampleMod.java
+++ src/main/java/com/github/xdonutzz/donutaddons/ExampleMod.java
@@ -1,11 +1,11 @@
-package com.example;
+package com.github.xdonutzz.donutaddons;
 
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = "examplemod", useMetadata=true)
+@Mod(modid = "donutaddons", useMetadata=true)
 public class ExampleMod {
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
